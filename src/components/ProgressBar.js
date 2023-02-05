0b78de4db4a6a4d88daf11fcf3534eabf5ade20f
konstantinosTabakis <konstantinos_tabakis@yahoo.com>
2023-02-05 22:15:00 +0200

    Adding progress bar

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,10 @@
+function ProgressBar({currentPage, lastPage}) {
+    return (
+        <div>
+            <progress min="0" max={lastPage} value={currentPage} />
+            
+        </div>
+    )
+}
+
+export default ProgressBar
